e26c442c7a6f5f2769c9df99ce505015cd84a2db
Erick Macias Vera <erickmacias161718@gmail.com>
2020-07-28 22:36:40 -0500

    mi primer commit

---
+++ envio-formulario.php
@@ -0,0 +1,22 @@
+<?php
+$nombre_usuario=$_POST['nombre'];
+$email_usuario=$_POST['email'];
+$mensaje_usuario=$_POST['mensaje'];
+//variables que toman lo q se envia por POST con su valor
+
+$destino="erickmacias161718@gmail.com";
+
+$asunto="consulta enviada desde www.CineriMavar29.com.ec";
+
+$mensaje.="Nombre: ".$nombre_usuario."\r\n";
+$mensaje.="Email: ".$email_usuario."\r\n";
+$mensaje.="Mensaje: ".$mensaje_usuario."\r\n";
+
+$remitente="From: algo@ce.com.ec";
+
+mail($destino, $asunto, $mensaje, $remitente);
+
+header("Location:indexCinthyaErick.php?i=ok");
+//si el formulario se envio correctamente, poner en la url despues del index.php un sigo de pregunta, una letra i y un igual ok
+
+?>
---
+++ indexCinthyaErick.php
@@ -0,0 +1,297 @@
+<!doctype html>
+<html lang="es">
+<head>
+<link rel="stylesheet" href="estilo.css">
+<meta charset="utf-8">
+<title>Cineri Mavar 29</title>
+<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="Owl-C/owl.carousel.min.css">
+<link rel="stylesheet" href="Qwl-C/owl.theme.default.min.css">
+<meta name="viewport" content="width=device-width, user-scalable=no">
+</head>
+
+<!--Estructura Body-->
+<body>
+
+<a id="comunicacion" href="" > <img src="imagenes/comunicator2.png" alt="imagen de cominucacion"></a>
+<!--<a id="comunicacion" href="" > <img src="imagenes/messenger.png" alt="imagen de messeger"></a>-->
+
+<a id="whatsapp" href="https://api.whatsapp.com/send?phone=593985715483&text= Hola! necesito mas informaciòn?"><img src="imagenes/icons8_whatsapp0_96px.png" alt="imagen de whatsApp"></a>
+
+<header>
+  <div class="menu">
+    <div class="contenedor">
+      <p class="logo">CINERIMAVAR29</p>
+      <img class="menu-icon" src="imagenes/menu2.png" alt="">
+      <nav>
+        <ul class="lista-menu">
+          <li><a href="#sobre-Nosotros">Sobre Nosotros</a></li>
+          <li><a href="#Servicios">Servicios</a></li>
+          <li><a href="#nuestro-trabajos">Mis Trabajos</a></li>
+          <li><a href="#porque-trabajar">¿Por que trabajar con nosotros?</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#que-esperas">Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+
+  <div class="contenedor" id="contenedor-titulo-flex">
+    <div class="contenedor-titulo">
+      <h1>Cineri Mavar 29</h1>
+      <h2>Ingenieros en Sistemas</h2>
+      <a href="">Ver Trabajos</a>
+    </div>
+  </div>
+</header>
+
+<!--Estructura Main-->
+<main>
+  <!--Sobre Nosotros-->
+  <section id="sobre-Nosotros">
+    <div class="contenedor">
+      <h3>Sobre Nosotros</h3>
+      <div class="contenedor-sobrenosotros">
+        <div class="computadoras">
+          <img src="imagenes/Compu4.png" alt="">
+        </div>
+        <div class="texto">
+          <p>Nuestros nombres son Cinthya Vargas Tapia
+          y Erick Macias Vera y somos Ingenieros en Sistemas.
+          Tenemos la esperiencia en reparar Computadoras, Programaciòn
+          y en el Desarrollo de sitios Web para pequeñas y medianas empresas,
+          hasta tiendas en linea con carrito de compras. </p>
+         <p>Nos aseguramos de utilizar siempre las ultimas Tecnologias de    DesarrolloWeb: HTML5, CSS3, jQuery, Wordpress. Si necesitas una pagina web moderna, funcional, que se adapte a todos los diferentes tipos de pantalla, te aseguro que llegaste al lugar correcto.</p>
+         <p>Puedes estar en contacto con Nosotros a traves de Facebook, WhatsApp o completando el formulario de contacto que se encuentra al final de mi sitio web.</p>
+         <a href="">VER TRABAJOS</a>
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!--Servicios-->
+<section id="Servicios">
+   <div class="contenedor">
+     <h3>Servicios</h3>
+     <div class="contenedor-servicios">
+       <div class="servicio violeta">
+         <h4>Diseño Web</h4>
+         <p>Páginas web atractivas y funcionales con el objetivo de satisfacer todas tus expectativas. La importancia del diseño es fundamental para tener éxito y atraer a tus visitantes. También lo es la velocidad de carga del sitio web, deben ser rápidos para que el visitante tenga la mejor experiencia.</p>
+         <img class="icono" src="imagenes/PC.png" alt="">
+         <img class="ondas" src="imagenes/wa.png" alt="">
+        </div>
+        <div class="servicio celeste">
+          <h4>Android Dev</h4>
+          <p>Hoy en día las páginas web se ven en multitud de dispositivos como tablets, celulares, libros electrónicos, notebooks, PCs, etc. La tecnología móvil es el presente y el futuro. Para eso es importante y me aseguro de que tu sitio web sea compatible con todos los tamaños de pantalla de esta época moderna.</p>
+          <img class="icono" src="imagenes/phone.png" alt="">
+          <img class="ondas" src="imagenes/wa.png" alt="">
+         </div>
+         <div class="servicio violeta">
+           <h4>Mantenimiento</h4>
+           <p>Al crear sitios web, tengo muy en cuenta la optimización de motores de búsqueda (SEO). Llevo a cabo prácticas de SEO estándar en cada página del sitio web, lo que permite que tu sitio tenga una clasificación más alta en los resultados de Google y asi generar mas tráfico a la web.</p>
+           <img class="icono" src="imagenes/mante.png" alt="">
+           <img class="ondas" src="imagenes/wa.png" alt="">
+          </div>
+          <div class="servicio celeste">
+            <h4>Marketing</h4>
+            <p>Es importante que puedas administrar tu página web vos mismo, desde cualquier lugar y en cualquier momento. Desarrollo sistemas de gestión de contenido que te permiten ver los datos de tu página web y editar el contenido que quieras, lo que te permite mantenerte actualizado.</p>
+            <img class="icono" src="imagenes/marketing.png" alt="">
+            <img class="ondas" src="imagenes/wa.png" alt="">
+           </div>
+           <div class="servicio violeta">
+             <h4>Diseño Grafico en Photoshop</h4>
+             <p>Photoshop se ha convertido en una potente herramienta dentro del mundo del diseño gráfico con la que poder desarrollar una gran variedad de funciones de forma intuitiva y accesible.</p>
+             <img class="icono" src="imagenes/marketing.png" alt="">
+             <img class="ondas" src="imagenes/wa.png" alt="">
+            </div>
+            <div class="servicio celeste">
+              <h4>Investigaciòn</h4>
+              <p>Utilizo las herramientas mas importantes para la investigacion como Google Academico, Google Libros y la Biblioteca Digital Mundial, para que tus investigaciones tengan excelente contenido para poner el conocimiento en manos de todos.</p>
+              <img class="icono" src="imagenes/marketing.png" alt="">
+              <img class="ondas" src="imagenes/wa.png" alt="">
+             </div>
+     </div>
+   </div>
+</section>
+
+<!--Carrucel-->
+<section id="nuestro-trabajos">
+  <div class="contenedor">
+    <h3>Nuestros Trabajos</h3>
+    <div class="owl-carousel owl-theme">
+    <div class="item"><a href="#"><img src="imagenes/sassy.jpg"></a><p>Proyecto 1</p></div>
+    <div class="item"><a href="#"><img src="imagenes/sassy.jpg"></a><p>Proyecto 2</p></div>
+    <div class="item"><a href="#"><img src="imagenes/Mantenimiento.png"></a><p>Proyecto 3</p></div>
+    <div class="item"><a href="#"><img src="imagenes/sassy.jpg"></a><p>Proyecto 4</p></div>
+    <div class="item"><a href="#"><img src="imagenes/sassy.jpg"></a><p>Proyecto 5</p></div>
+    <div class="item"><a href="#"><img src="imagenes/Mantenimiento.png"></a><p>Proyecto 6</p></div>
+    </div>
+  </div>
+</section>
+
+<!--Trabajar conmigo-->
+<section id="porque-trabajar">
+  <div class="contenedor">
+    <h3>¿Por que trabajar con Nosotros?</h3>
+    <div class="contenedor-trabajar">
+      <div class="items">
+        <ul>
+          <li><img src="imagenes/velocidad.png" alt=""></li>
+          <li>Velocidad</li>
+          <li>Nos aseguramos de que su web cargue entre 2 y 4 segundos (depende de la cantidad de contenido del sitio). Si una web tarda más de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar más</li>
+        </ul>
+      </div>
+      <div class="items">
+        <ul>
+          <li><img src="imagenes/mantenimiento2.png" alt=""></li>
+          <li>Mantenimiento</li>
+          <li>Mantenemos una comunicación constante con mi cliente, para enseñarle a modificar contenido mediante el panel de Wordpress o para cualquier duda/cambio que quiera hacer en su sitio web</li>
+        </ul>
+      </div>
+      <div class="items">
+        <ul>
+          <li><img src="imagenes/mobile.png" alt=""></li>
+          <li>Mobile Priority</li>
+          <li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo móvil</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--publicaciones-->
+<section id="blog">
+  <div class="contenedor">
+    <h3>Blog</h3>
+    <div class="contenedor-publicaciones">
+      <div class="publicacion">
+        <img src="imagenes/desarrollador.jpg" alt="">
+        <div class="contenido">
+          <h4>No alcanza con tener solamente una pagina web</h4>
+          <p>¿Queres aprender cuales son las herramientas que usan las mejores marcas para generar trafico?</p>
+          <a href="#">LEER MAS →</a>
+        </div>
+      </div>
+      <div class="publicacion">
+        <img src="imagenes/blog3.jpg" alt="">
+        <div class="contenido">
+          <h4>¿Que es el remarketing y como utilizarlo?</h4>
+          <p>El remarketing es una de las tecnicas mas utilizadas por las mejores compañias!
+          ¿Queres saber de que se trata?</p>
+          <a href="#">LEER MAS →</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--Que esperas tener pagina-->
+<section id="que-esperas">
+  <div class="contenedor">
+    <h3>¿Que esperas para tener tu pagina web?</h3>
+    <!--<a href="#">CHARLEMOS</a>-->
+  </div>
+</section>
+
+<!--Contactos-->
+<section id="contacto">
+  <?php
+      if (@$_GET['i']=='ok') {
+        // el formulario se envio correctamente ?>
+      <h3>Los datos fueron enviado correctamente.
+        Nos contactamos en breve.</h3>
+      <?php
+      } else {
+      // code...
+      ?>
+  <div class="contenedor">
+    <h3>Contacto</h3>
+    <form class="" action="envio-formulario.php" method="post" enctype="multipart/form-data">
+      <input type="text" placeholder="Nombre" name="nombre" required>
+      <input type="email" placeholder="Email" name="email" required>
+      <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+      <input type="submit" name="" value="ENVIAR MENSAJE">
+    </form>
+          <?php } ?>
+
+    <div class="contacto-info">
+      <div class="mail"><img src="imagenes/icons8_gmail_96px.png" alt="">erickmacias161718@gmail.com</div>
+      <div class="whatsApp"><img src="imagenes/icons8_whatsapp0_96px.png" alt="">0985715483-0988994210</div>
+    </div>
+  </div>
+</section>
+</main>
+
+<footer>
+  <div class="contenedor">
+    <div class="redes">
+      <a href="https://www.facebook.com/Cineri-107641207496116/?modal=admin_todo_tour"><img src="imagenes/icons8_facebook_96px.png" alt="imagen de facebook"> </a>
+      <a href="https://www.instagram.com/erick_macias_16_cye/?hl=es-la"><img src="imagenes/icons8_instagram_new_96px.png" alt="imagen de instagram"> </a>
+      <a href="https://github.com/ErickMacias29/Macias"><img src="imagenes/icons8_github_96px.png" alt="imagen de github"> </a>
+    </div>
+    <div class="parrafo">
+      <p>@ 2020 Erick Macias Vera | Dieñador Web Ecuador. Todos los derechos reservados</p>
+    </div>
+  </div>
+</footer>
+
+<!--Estructura <script>-->
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="Owl-C/owl.carousel.min.js"></script>
+<script>
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+</script>
+
+<script>
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $('.menu').css({"position":"fixed"});
+      $('.menu').css({"width":"100%"});
+      $('.menu').css({"top":"0"});
+      $('.menu').css({"background":"#fff"});
+      $('.menu a').css({"color":"#000"});
+      $('.logo').css({"color":"#000"});
+      $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+      $('.menu').css({"z-index":"100"});
+    }else {
+      //cuando no hay scrollTop
+      $('.menu').css({"position":"relative"});
+      $('.menu').css({"background":"transparent"});
+      $('.menu').css({"box-shadow":"0 0 0"});
+      $('.menu a').css({"color":"#fff"});
+      $('.logo').css({"color":"#fff"});
+    }
+  })
+
+$('.menu-icon').click(function(){
+  $('header nav').slideToggle();
+})
+
+})
+</script>
+
+</body>
+</html>
